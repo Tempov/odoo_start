ffad1c10eccbaf601be91501e76f57486675a8b9
Artem Ivashchenko <mr.tempov@gmail.com>
2025-01-29 16:35:18 +0200

    [ADD] module_name

---
+++ module_name/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "*** Module name *** ",
+    "author": "",
+    "website": "",
+    "support": "",
+    "version": "1.0.0",
+    "category": "",
+    "description": """ """,
+    "depends": [],
+    "data": [],
+    "images": [],
+    "installable": True,
+    "application": False,
+    "autoinstall": False,
+    "license": "OPL-1"
+}
